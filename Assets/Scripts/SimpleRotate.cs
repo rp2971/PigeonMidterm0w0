12d005e7ee9bd5a5debf6388dd645a2db83e3be2
Raveena Panja <rp2971@nyu.edu>
2019-10-20 17:09:06 -0400

    StartScene to GameScene code + visual placement

---
+++ Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(transform.up * rotationSpeed);
+    }
+}
